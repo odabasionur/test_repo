376e17e9e9559a5d2e23cb3e04b3af04b0f00aa1
Onur <odabasionur3@gmail.com>
2020-08-07 10:38:47 +0300

    Commit 2.3

--- test_script.py
+++ test_script.py
@@ -1,3 +1,8 @@
-# branch_never_exists local de var ama githubda yok bakalım ekleyecek mi ? master to master push.  "Commit 2.1"
+# branch_never_exists local de var ama githubda yok bakalım ekleyecek mi ? master to master push.
+# "Commit 2.1"
 
-# branch_never_exists de değişiklik yapıp pushlarken branche master veriyorum.  "Commit 2.2"
+# branch_never_exists de değişiklik yapıp pushlarken branche master veriyorum.
+# "Commit 2.2" Everything up-to-date dedi.
+
+# branch_never_exists de değişiklik duruyor ve bu satır ekleniyor. branche branch_never_exists veriyorum.
+# "Commit 2.3" olarak gelmeli
