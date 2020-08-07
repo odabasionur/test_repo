da44edce4c49de90bfc3bc471531949455aeb5ab
Onur <odabasionur3@gmail.com>
2020-08-07 10:21:41 +0300

    Commit 2.2

--- test_script.py
+++ test_script.py
@@ -1,1 +1,3 @@
-# branch_never_exists local de var ama githubda yok bakalım ekleyecek mi ? master to master push
+# branch_never_exists local de var ama githubda yok bakalım ekleyecek mi ? master to master push.  "Commit 2.1"
+
+# branch_never_exists de değişiklik yapıp pushlarken branche master veriyorum.  "Commit 2.2"
