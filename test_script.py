9b6f2f2c26d42911878dd78468dfc498bfcb8957
Onur <odabasionur3@gmail.com>
2020-08-07 10:57:23 +0300

    Commit 2.4 master

--- test_script.py
+++ test_script.py
@@ -1,1 +1,14 @@
-# branch_never_exists local de var ama githubda yok bakalım ekleyecek mi ? master to master push
+# branch_never_exists local de var ama githubda yok bakalım ekleyecek mi ? master to master push.
+# "Commit 2.1"
+
+# branch_never_exists de değişiklik yapıp pushlarken branche master veriyorum.
+# "Commit 2.2" Everything up-to-date dedi.
+
+# branch_never_exists de değişiklik duruyor ve bu satır ekleniyor. branche branch_never_exists veriyorum.
+# "Commit 2.3" olarak gelmeli
+# Githubda Yeni bir branch oluşturdu mastera atamadım. Commit 2.3.1
+
+# Sadece test_script.py dosyasını mastera mergeliyorum ve master'ı pushluyorum.
+# > git checkout master
+# > git checkout branch_never_exists test_script.py
+# "Commit 2.4" ile master'a eklenmeli
